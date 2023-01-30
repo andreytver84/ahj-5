3d40d182e06247c287140fd127a36b63805aadd3
Андрей Увиков <uvikmedium@mail.ru>
2023-01-30 19:34:29 +0300

    change structure of files js

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,5 @@
-import { popover } from "./popover";
+import "./popoverhandler";
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log("app.js included");
-
-    const popoversBtn = document.querySelectorAll('.btn-popover');
-    popoversBtn.forEach(item => item.addEventListener('click', (e) => {
-        const popoverBlock = document.querySelector('.popover');
-        if (!popoverBlock) {            
-            popover(e.target);
-            console.log('1');
-        } else if (popoverBlock && e.target.getAttribute('title') === popoverBlock.getAttribute('data-title')) {
-            popoverBlock.remove();
-        } else if (popoverBlock) {
-            console.log('3');
-            popoverBlock.remove();
-            popover(e.target);
-        }
-    }));
-
-});
+console.log("app.js included");
 
 
---
+++ src/js/popoverhandler.js
@@ -0,0 +1,17 @@
+import { popover } from "./popover";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const popoversBtn = document.querySelectorAll('.btn-popover');
+    popoversBtn.forEach(item => item.addEventListener('click', (e) => {
+        const popoverBlock = document.querySelector('.popover');
+        if (!popoverBlock) {            
+            popover(e.target);
+        } else if (popoverBlock && e.target.getAttribute('title') === popoverBlock.getAttribute('data-title')) {
+            popoverBlock.remove();
+        } else if (popoverBlock) {
+            popoverBlock.remove();
+            popover(e.target);
+        }
+    }));
+
+});
