d07f447fea42de5ac24899630222de271712d35a
Андрей Увиков <uvikmedium@mail.ru>
2023-02-04 17:07:21 +0300

    upd popover and remove test on deploy

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,11 +1,13 @@
 export function popover(el) {
-  const { top } = el.getBoundingClientRect();
+  const { top, left, right } = el.getBoundingClientRect();
   const topPopover = top - 120;
-  console.log(topPopover);
+  const leftPopover = left - (300 - (right - left)) / 2;
   const elTitle = el.getAttribute("title");
   const elContent = el.getAttribute("data-content");
   const popoverBlock = `
-    <div class="popover" style="top: ${topPopover}px" data-title="${elTitle}">
+    <div class="popover" style="top: ${topPopover}px; left: ${
+    leftPopover >= 0 ? leftPopover : 0
+  }px" data-title="${elTitle}">
         <div class="popover-cont" >
             <div class="arrow"></div>
             <h3 class="popover-header">${elTitle}</h3>
